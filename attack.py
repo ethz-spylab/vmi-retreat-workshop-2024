ec7c5390d9990a424375f2e98ef98f4676456a8e
Edoardo Debenedetti <edoardo.debenedetti@inf.ethz.ch>
2024-09-08 17:22:28 +0200

    Initial commit

---
+++ attack.py
@@ -0,0 +1,12 @@
+from agentdojo.attacks import FixedJailbreakAttack
+
+
+class CustomAttack(FixedJailbreakAttack): ...
+
+
+def main():
+    print("Hello from vmi-retreat-workshop-2024!")
+
+
+if __name__ == "__main__":
+    main()
